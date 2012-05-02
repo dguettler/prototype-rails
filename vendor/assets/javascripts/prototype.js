e794e1fe915af04809f3f7290da1e9249e6a034f
Daniel Guettler <daniel.guettler@gmail.com>
2012-05-01 23:35:59 -0400

    - save orginal position for dnd

--- vendor/assets/javascripts/prototype.js
+++ vendor/assets/javascripts/prototype.js
@@ -3793,10 +3793,11 @@ Element.addMethods({
     var layout = element.getLayout();
 
     element.store('prototype_absolutize_original_styles', {
-      left:   element.getStyle('left'),
-      top:    element.getStyle('top'),
-      width:  element.getStyle('width'),
-      height: element.getStyle('height')
+      position: element.getStyle('position'),
+      left:     element.getStyle('left'),
+      top:      element.getStyle('top'),
+      width:    element.getStyle('width'),
+      height:   element.getStyle('height')
     });
 
     element.setStyle({
